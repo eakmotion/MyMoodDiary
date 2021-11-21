e6647495b42110158b44019e3a8efe5f84c94e8e
eakmotion <eakpun@gmail.com>
2021-11-22 03:25:56 +0700

    Add a pie chart and show mood insights

--- src/screens/Stats.screen.tsx
+++ src/screens/Stats.screen.tsx
@@ -1,10 +1,36 @@
 import React, { FC } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { useAppContext } from '../App.provider';
+import { VictoryPie } from 'victory-native';
+import { theme } from '../theme';
+import groupBy from 'lodash/groupBy';
 
 export const Stats: FC = () => {
+  const appContext = useAppContext();
+
+  const data = Object.entries(groupBy(appContext.moodList, 'mood.emoji')).map(
+    ([key, value]) => ({
+      x: key,
+      y: value.length,
+    }),
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Stats</Text>
+      <VictoryPie
+        labelRadius={80}
+        radius={150}
+        innerRadius={50}
+        colorScale={[
+          theme.colorPurple,
+          theme.colorLavender,
+          theme.colorBlue,
+          theme.colorGrey,
+          theme.colorWhite,
+        ]}
+        style={{ labels: { fontSize: 30 } }}
+        data={data}
+      />
     </View>
   );
 };
@@ -12,5 +38,7 @@ export const Stats: FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
