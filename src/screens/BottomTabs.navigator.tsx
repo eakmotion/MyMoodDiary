a4d68cbed7555e356edbc66be55edfa030c80382
eakmotion <eakpun@gmail.com>
2021-11-22 02:55:39 +0700

    Add icons to the bottom tabs

--- src/screens/BottomTabs.navigator.tsx
+++ src/screens/BottomTabs.navigator.tsx
@@ -3,12 +3,34 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Home } from './Home.screen';
 import { History } from './History.screen';
 import { Stats } from './Stats.screen';
+import { StatsIcon, HistoryIcon, HomeIcon } from '../components/Icons';
+import { theme } from '../theme';
 
 const BottomTabs = createBottomTabNavigator();
 
 export const BottomTabsNavigator: FC = () => {
   return (
-    <BottomTabs.Navigator>
+    <BottomTabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: theme.colorBlue,
+        tabBarInactiveTintColor: theme.colorGrey,
+        tabBarShowLabel: false,
+        tabBarIcon: ({ color, size }) => {
+          if (route.name === 'Home') {
+            return <HomeIcon color={color} size={size} />;
+          }
+
+          if (route.name === 'History') {
+            return <HistoryIcon color={color} size={size} />;
+          }
+
+          if (route.name === 'Stats') {
+            return <StatsIcon color={color} size={size} />;
+          }
+
+          return null;
+        },
+      })}>
       <BottomTabs.Screen name="Home" component={Home} />
       <BottomTabs.Screen name="History" component={History} />
       <BottomTabs.Screen name="Stats" component={Stats} />
